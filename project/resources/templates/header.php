c10cb32db9e82914bc86c224edecb3b80a35e73b
Jose Augusto <jose.bolina@neppoadm.com.br>
2018-09-24 16:10:19 -0300

    separate css files

--- project/resources/templates/header.php
+++ project/resources/templates/header.php
@@ -1,11 +1,6 @@
 <?php
     require_once (realpath(dirname(__FILE__)) . "/../configuration.php");
     require_once (LIBRARY_PATH . '/own/renderTemplates.php');
-
-    if (isset($_GET['page'])) {
-        renderLayoutWithContent($_GET['page']);
-    }
-
 ?>
 
 <nav id="header" class="navbar navbar-expand-lg fixed-top background-slate">
