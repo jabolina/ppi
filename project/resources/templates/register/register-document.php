7e65bb1c243004680fba4528e41f18a1aa500980
Jose Augusto <jose.bolina@neppoadm.com.br>
2018-09-24 23:59:08 -0300

    fixes and changes rsrsrsrsr

--- project/resources/templates/register/register-document.php
+++ project/resources/templates/register/register-document.php
@@ -4,7 +4,7 @@
     <div class="row form-group">
         <label for="cpf" class="col-md-2 col-form-label">CPF:</label>
         <div class="col-md-9">
-            <input type="number" class="form-control" id="cpf" name="cpf">
+            <input type="number" class="form-control" id="cpf" name="cpf" maxlength="11">
         </div>
     </div>
 
